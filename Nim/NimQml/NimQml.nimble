fcbc2662f045e13388ca2b877570a4aa093bf117
Filippo Cucchetto <filippocucchetto@gmail.com>
2014-12-29 15:32:53 +0100

    Moved the NimQml to a separate directory and added .nimble file

---
+++ Nim/NimQml/NimQml.nimble
@@ -0,0 +1,9 @@
+[Package]
+name          = "NimQml"
+version       = "0.01"
+author        = "Filippo Cucchetto"
+description   = "QML bindings for Nimrod"
+license       = "GPLv3"
+
+[Deps]
+Requires: "nimrod >= 0.10.2"
--- Nim/cmake/Nim/UseNim.cmake
+++ Nim/cmake/Nim/UseNim.cmake
@@ -1,24 +1,32 @@
 include(CMakeParseArguments)
 
 function(add_nim_executable )
-    cmake_parse_arguments(ARGS "" "TARGET" "SOURCES" ${ARGN})
+  set(options )
+  set(oneValueArgs TARGET)
+  set(multiValueArgs SOURCES PATHS)
+  cmake_parse_arguments(ARGS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+  
+  # collect set of input source files
+  set(in_files "")
+  foreach(src ${ARGS_SOURCES} ${ARGS_UNPARSED_ARGUMENTS})
+    list(APPEND in_files "${CMAKE_CURRENT_SOURCE_DIR}/${src}")
+  endforeach(src ${ARGS_SOURCES} ${ARGS_UNPARSED_ARGUMENTS})
 
-    # collect set of input source files
-    set(in_files "")
-    foreach(src ${ARGS_SOURCES} ${ARGS_UNPARSED_ARGUMENTS})
-        list(APPEND in_files "${CMAKE_CURRENT_SOURCE_DIR}/${src}")
-    endforeach(src ${ARGS_SOURCES} ${ARGS_UNPARSED_ARGUMENTS})
+  set(in_paths "")
+  foreach(path ${ARGS_PATHS} ${ARGS_UNPARSED_ARGUMENTS})
+    list(APPEND in_paths "--path:${CMAKE_CURRENT_SOURCE_DIR}/${path}")
+  endforeach(path ${ARGS_PATHS} ${ARGS_UNPARSED_ARGUMENTS})
+  
+  # set the target binary and nim cache directory
+  set(nim_target "${CMAKE_CURRENT_BINARY_DIR}/${ARGS_TARGET}")
+  set(DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/nimcache")
 
-    # set the target binary and nim cache directory
-    set(nim_target "${CMAKE_CURRENT_BINARY_DIR}/${ARGS_TARGET}")
-    set(DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/nimcache")
-
-    # add target to trigger the nimrod compiler
-    add_custom_target(
-    	nim ALL
-    	COMMAND
-        	${NIM_EXECUTABLE} "c" "--nimcache:" ${DIRECTORY} "--out:" ${nim_target} ${in_files}
-    	DEPENDS
-        	${in_files}
-    )
+  # add target to trigger the nimrod compiler
+  add_custom_target(
+      nim ALL
+      COMMAND
+	      ${NIM_EXECUTABLE} "c" ${in_paths} "--nimcache:" ${DIRECTORY} "--out:" ${nim_target} ${in_files} 
+      DEPENDS
+	      ${in_files}
+  )
 endfunction(add_nim_executable)
